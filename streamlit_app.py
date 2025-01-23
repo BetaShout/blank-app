7ac771d9d77c227ebffdac7425bfc0b9a924f1a8
BetaShout <112172536+BetaShout@users.noreply.github.com>
2025-01-23 21:10:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,61 @@
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Music Filter")
+ACCESS_TOKEN = "qtqL3RCmEvqPiolzQRJtXnQIcHdM0YGYan1rmB4O_9oQN71tmiNDMYH2hvIxM0Tw"
+GENIUS_API_URL = "https://api.genius.com"
+
+st.title("Müzik Filtresi V1")
+st.write("## Şarkı Arama V1")
+st.write("Anahtar Kelimesini Girdiğin Şarkının İsmini ve Genius.com Linkini Veren Uygulama")
+keyword = st.text_input("Lütfen Anahtar Kelimeleri Giriniz. / İsteğe Göre Girdiğiniz Şarkıcının Şarkılarınıda Gösterir.")
+
+
+def music_filter(keyword):
+    headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}  # Authorization header'ını düzeltme
+    search_url = f"{GENIUS_API_URL}/search"
+    params = {"q": keyword}  # params'ı düzgün şekilde kullanıyoruz
+    response = requests.get(search_url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        hits = response.json()["response"]["hits"]
+        return [{"title": hit["result"]["title"], "url": hit["result"]["url"]} for hit in hits]
+    else:
+        st.error("URL Alınamadı. Tekrar Deneyiniz.")
+        return []
+
+
+def sarki_sozleri(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # Şarkı sözlerini içeren farklı HTML etiketleri kontrol ediliyor
+        lyrics_div = soup.find("div", class_="lyrics")
+        if not lyrics_div:
+            lyrics_div = soup.find("div", class_="Lyrics_Container")
+
+        # Eğer 'lyrics' veya 'Lyrics_Container' sınıfları yoksa, alternatif etiketlere bakıyoruz
+        if not lyrics_div:
+            lyrics_div = soup.find("p")
+
+        if lyrics_div:
+            return lyrics_div.get_text(separator="\n")
+        else:
+            return "Şarkı Sözleri Bulunamadı"
+    else:
+        return "Şarkı Sözleri Çekilemedi"
+
+
+if keyword:
+    results = music_filter(keyword)
+    if results:
+        st.subheader("Arama Sonuçları")
+        for result in results:
+            if st.button(f"{result['title']} "):
+                lyrics = sarki_sozleri(result["url"])
+                link_html = f'<a href="{result["url"]}" target="_blank" style="font-size: 20px; font-weight: bold; color: #1E90FF;">Şarkı Sözlerini Görmek İçin Tıklayınız👀</a>'
+                st.markdown(link_html, unsafe_allow_html=True)
+    else:
+        st.error("Sonuç Bulunamadı!")
