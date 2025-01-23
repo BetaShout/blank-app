be7b40317996f72f9ecbafd00ec9b1c918c56817
BetaShout <112172536+BetaShout@users.noreply.github.com>
2025-01-23 21:57:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,11 @@ st.write("## Şarkı Arama V1")
 st.write("Anahtar Kelimesini Girdiğin Şarkının İsmini ve Genius.com Linkini Veren Uygulama")
 st.write("Daha iyi sonuçlar elde etmek için daha spesifik şarkı sözleri belirleyebilirsiniz.")
 keyword = st.text_input("Lütfen Anahtar Kelimeleri Giriniz. / İsteğe Göre Girdiğiniz Şarkıcının Şarkılarınıda Gösterir.")
-st.radio("Beğendin mi?", ["Evet ", "Evet"])
+secim = st.radio("Beğendin mi ?",
+                ("Evet","Hayır")
+)
+if secim == "Hayır":
+    st.error("Yanlış Seçenek")
 
 def music_filter(keyword):
     headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}  # Authorization header'ını düzeltme
