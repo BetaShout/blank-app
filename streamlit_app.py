8110671b36e86d353ae83cdd2f82f13e96ed774d
BetaShout <112172536+BetaShout@users.noreply.github.com>
2025-01-23 22:10:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write("## Şarkı Arama V1")
 st.write("Anahtar Kelimesini Girdiğin Şarkının İsmini ve Genius.com Linkini Veren Uygulama")
 st.write("Daha iyi sonuçlar elde etmek için daha spesifik şarkı sözleri belirleyebilirsiniz.")
 keyword = st.text_input("Lütfen Anahtar Kelimeleri Giriniz. / İsteğe Göre Girdiğiniz Şarkıcının Şarkılarınıda Gösterir.")
-secim = st.radio("Beğendin mi ?",
+secim = st.radio(key=secim1,"Beğendin mi ?",
                 ("Geliştirilebilir","Evet","Hayır")
 )
 if secim == "Hayır":
