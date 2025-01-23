c894f0220d702e3e2ef1b6d5ba5664744c42bb7f
BetaShout <112172536+BetaShout@users.noreply.github.com>
2025-01-23 22:19:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,22 +46,14 @@ def sarki_sozleri(url):
 
 
 if keyword:
-    results = music_filter(keyword)  # Anahtar kelime ile arama yap
+    results = music_filter(keyword)
     if results:
-        st.subheader("Arama Sonuçları")  # Arama sonuçlarının başlığı
-        for index, result in enumerate(results):  # Her bir şarkı sonucu için döngü
+        st.subheader("Arama Sonuçları")
+        for result in results:
             if st.button(f"{result['title']}", key=f"button_{index}"):  # Benzersiz bir key ile buton
-                # Şarkı sözlerini çekme
                 lyrics = sarki_sozleri(result["url"])
-                
-                if lyrics:  # Eğer şarkı sözleri başarıyla çekilmişse
-                    st.subheader(result["title"])  # Şarkı başlığını göster
-                    st.text(lyrics)  # Şarkı sözlerini göster
-                    
-                    # Genius sayfasına yönlendiren bir bağlantı oluştur
-                    link_html = f'<a href="{result["url"]}" target="_blank" style="font-size: 20px; font-weight: bold; color: #1E90FF; text-decoration: none;">Şarkı Sözlerini Görmek İçin Tıklayınız 👀</a>'
-                    st.markdown(link_html, unsafe_allow_html=True)
-
+                link_html = f'<a href="{result["url"]}" target="_blank" style="font-size: 20px; font-weight: bold; color: #1E90FF;">Şarkı Sözlerini Görmek İçin Tıklayınız👀</a>'
+                st.markdown(link_html, unsafe_allow_html=True)
     else:
-        st.error("Sonuç bulunamadı!")  # Eğer hiçbir sonuç yoksa hata mesajı
+        st.error("Sonuç Bulunamadı!")
 
